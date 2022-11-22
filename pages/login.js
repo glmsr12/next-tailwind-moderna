008a42d17e037ca8b606084075472963dbb85758
glmsr12 <gulumsercakmak@hotmail.com>
2022-11-22 15:41:09 -0800

    "update files"

--- pages/login.js
+++ pages/login.js
@@ -64,34 +64,11 @@ export default function LoginScreen() {
             <div className="text-red-500">{errors.email.message}</div>
           )}
         </div>
-<<<<<<< HEAD
-=======
-        <div className="mb-4">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            {...register('password', {
-              required: 'Please enter password',
-              minLength: { value: 6, message: 'password is more than 5 chars' },
-            })}
-            className="w-full"
-            id="password"
-            autoFocus
-          ></input>
-          {errors.password && (
-            <div className="text-red-500 ">{errors.password.message}</div>
-          )}
-        </div>
->>>>>>> b2419482b9c07c51cf4b2d99960ee3a872d8af42
         <div className="mb-4 ">
           <button className="primary-button">Login</button>
         </div>
         <div className="mb-4 ">
-<<<<<<< HEAD
           Don&apos;t have an account? &nbsp;
-=======
-          Do not have an account?
->>>>>>> b2419482b9c07c51cf4b2d99960ee3a872d8af42
           <Link href={`/register?redirect=${redirect || '/'}`}>Register</Link>
         </div>
       </form>
