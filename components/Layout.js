d5999fb81d872f0ea0b5c62f54825407121bc2dc
glmsr12 <gulumsercakmak@hotmail.com>
2022-11-22 14:50:49 -0800

    "update files"

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ export default function Layout({ title, children }) {
   const [cartItemsCount, setCartItemsCount] = useState(0);
   useEffect(() => {
     setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
-  }, []);
+  }, [cart.cartItems]);
   return (
     <>
       <Head>
