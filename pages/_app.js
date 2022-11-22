6053cad25254cd352ea20373d4bbc31c4a21c78d
glmsr12 <gulumsercakmak@hotmail.com>
2022-11-22 15:28:06 -0800

    "update changes"

--- pages/_app.js
+++ pages/_app.js
@@ -5,19 +5,11 @@ import { StoreProvider } from '../utils/Store';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-<<<<<<< HEAD
     <StoreProvider session={session}>
       <StoreProvider>
         <Component {...pageProps} />
       </StoreProvider>
     </StoreProvider>
-=======
-    <SessionProvider session={session}>
-      <StoreProvider>
-        <Component {...pageProps} />
-      </StoreProvider>
-    </SessionProvider>
->>>>>>> b2419482b9c07c51cf4b2d99960ee3a872d8af42
   );
 }
 
