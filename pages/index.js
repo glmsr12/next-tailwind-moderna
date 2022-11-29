f5409e459e422987fdbc9e6b42572f67449be280
glmsr12 <gulumsercakmak@hotmail.com>
2022-11-28 20:36:30 -0800

    "update files"

--- pages/index.js
+++ pages/index.js
@@ -1,86 +1,50 @@
-/* eslint-disable @next/next/no-img-element */
-import NextLink from 'next/link';
-import { Grid, Link, Typography } from '@mui/material';
-import Layout from '../components/Layout';
-import db from '../utils/db';
-import Product from '../models/Product';
 import axios from 'axios';
-import { useRouter } from 'next/router';
 import { useContext } from 'react';
-import { Store } from '../utils/Store';
+import { toast } from 'react-toastify';
+import Layout from '../components/Layout';
 import ProductItem from '../components/ProductItem';
-import Carousel from 'react-material-ui-carousel';
-import useStyles from '../utils/styles';
+import Product from '../models/Product';
+import db from '../utils/db';
+import { Store } from '../utils/Store';
 
-export default function Home(props) {
-  const classes = useStyles();
-  const router = useRouter();
+export default function Home({ products }) {
   const { state, dispatch } = useContext(Store);
-  const { topRatedProducts, featuredProducts } = props;
+  const { cart } = state;
+
   const addToCartHandler = async (product) => {
-    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
+    const existItem = cart.cartItems.find((x) => x.slug === product.slug);
     const quantity = existItem ? existItem.quantity + 1 : 1;
     const { data } = await axios.get(`/api/products/${product._id}`);
+
     if (data.countInStock < quantity) {
-      window.alert('Sorry. Product is out of stock');
-      return;
+      return toast.error('Sorry. Product is out of stock');
     }
     dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
-    router.push('/cart');
+
+    toast.success('Product added to the cart');
   };
+
   return (
-    <Layout>
-      <Carousel className={classes.mt1} animation="slide">
-        {featuredProducts.map((product) => (
-          <NextLink
-            key={product._id}
-            href={`/product/${product.slug}`}
-            passHref
-          >
-            <Link>
-              <img
-                src={product.featuredImage}
-                alt={product.name}
-                className={classes.featuredImage}
-              ></img>
-            </Link>
-          </NextLink>
+    <Layout title="Home Page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {products.map((product) => (
+          <ProductItem
+            product={product}
+            key={product.slug}
+            addToCartHandler={addToCartHandler}
+          ></ProductItem>
         ))}
-      </Carousel>
-      <Typography variant="h5">Popular Products</Typography>
-      <Grid container spacing={3}>
-        {topRatedProducts.map((product) => (
-          <Grid item md={4} key={product.name}>
-            <ProductItem
-              product={product}
-              addToCartHandler={addToCartHandler}
-            />
-          </Grid>
-        ))}
-      </Grid>
+      </div>
     </Layout>
   );
 }
 
 export async function getServerSideProps() {
   await db.connect();
-  const featuredProductsDocs = await Product.find(
-    { isFeatured: true },
-    '-reviews'
-  )
-    .lean()
-    .limit(3);
-  const topRatedProductsDocs = await Product.find({}, '-reviews')
-    .lean()
-    .sort({
-      rating: -1,
-    })
-    .limit(9);
-  await db.disconnect();
+  const products = await Product.find().lean();
   return {
     props: {
-      featuredProducts: featuredProductsDocs.map(db.convertDocToObj),
-      topRatedProducts: topRatedProductsDocs.map(db.convertDocToObj),
+      products: products.map(db.convertDocToObj),
     },
   };
 }
