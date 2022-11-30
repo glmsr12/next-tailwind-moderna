e1e3633db12d8c570caef622019def335fec67b1
Gulumser C <91402082+glmsr12@users.noreply.github.com>
2022-11-29 23:07:23 -0800

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -27,7 +27,7 @@ export default function Layout({ title, children }) {
   return (
     <>
       <Head>
-        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
+        <title>{title ? title + ' - Moderna' : 'Moderna'}</title>
         <meta name="description" content="Ecommerce Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -38,7 +38,7 @@ export default function Layout({ title, children }) {
         <header>
           <nav className="flex h-12 items-center px-4 justify-between shadow-md">
             <Link href="/">
-              <a className="text-lg font-bold">amazona</a>
+              <a className="text-lg font-bold">Moderna</a>
             </Link>
             <div>
               <Link href="/cart">
