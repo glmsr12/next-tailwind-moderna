e356f7608d82a4c3f6d696199da607ccc8014b97
glmsr12 <gulumsercakmak@hotmail.com>
2022-12-03 00:04:09 -0800

    "update file"

--- pages/admin/users.js
+++ pages/admin/users.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import Link from 'next/link';
 import React, { useEffect, useReducer } from 'react';
 import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Layout from '../../components/Layout';
 import { getError } from '../../utils/error';
 
